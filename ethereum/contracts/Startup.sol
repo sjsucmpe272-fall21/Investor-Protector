03005d06cf3c82f26c561ac8c0ee8adfa25ed46b
tojuna <anujotsingh@Anujots-MacBook-Air.local>
2021-11-10 22:37:15 -0800

    additions to contract

--- ethereum/contracts/Startup.sol
+++ ethereum/contracts/Startup.sol
@@ -1,9 +1,22 @@
 pragma solidity ^0.4.17;
 
 contract Startup {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+    
+    Request[] public requests;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+    
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
     
     function Startup(uint minimum) public {
         manager = msg.sender;
@@ -13,5 +26,16 @@ contract Startup {
     function contribute() public payable {
         require(msg.value > minimumContribution);
         approvers.push(msg.sender);
+    }
+    
+    function createRequest(string description, uint value, 
+    address recipient) public restricted {
+        Request newRequest = Request({
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false
+        });
+        requests.push(newRequest);
     }
 }
