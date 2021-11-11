163bb8ee7f94ca265c5f1683969b9339128ff7fb
tojuna <anujotsingh@Anujots-MacBook-Air.local>
2021-11-11 14:15:11 -0800

    testing env setup

---
+++ test/Startup.test.js
@@ -0,0 +1,29 @@
+const assert = require('assert')
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const compiledFactory = require('../ethereum/build/StartupFactory.json');
+const compiledStartup = require('../ethereum/build/Startup.json');
+
+let accounts;
+let factory;
+let startupAddress;
+let startup;
+
+beforeEach(async () => {
+	accounts = await web3.eth.getAccounts();
+	factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+	.deploy({data: compiledFactory.bytecode}).send({from: accounts[0], gas: '1000000'})
+
+	await factory.methods.createStartup('100').send({
+		from: accounts[0], 
+		gas: '1000000'
+	});
+
+	[startupAddress] = await factory.methods.getDeployedStartups().call();
+	startup = await new web3.eth.Contract(
+		JSON.parse(compiledStartup.interface),
+		startupAddress
+	);
+})
