37cc8ccdad21d35432b10d292cf6977704a7e160
tojuna <anujotsingh@Anujots-MacBook-Air.local>
2021-11-11 18:51:06 -0800

    more tests

--- test/Startup.test.js
+++ test/Startup.test.js
@@ -31,13 +31,44 @@ beforeEach(async () => {
 });
 
 describe('Startups', () => {
-  it('deploys a factory and a startup', () => {
+  it('Deploy a StartupFactory and a Startup', () => {
     assert.ok(factory.options.address);
     assert.ok(startup.options.address);
   });
 
-  it('marks caller as the startup manager', async () => {
+  it('Mark caller as the Startup manager', async () => {
   	const manager = await startup.methods.manager().call();
   	assert.equal(accounts[0], manager);
+  });
+
+  it('Allow people to contribute money and marks them as approvers', async () =>{
+    await startup.methods.contribute().send({
+      value: '200',
+      from: accounts[1]
+    });
+    const isContributor = await startup.methods.approvers(accounts[1]).call();
+    assert(isContributor);
+  });
+
+  it('Require minimum contribution', async () => {
+    try {
+      await startup.methods.contribute().send({
+        value: '5',
+        from: accounts[1]
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('Allows manager to make a payment request', async() => {
+    await startup.methods.createRequest('Buy shoes', '100', accounts[1])
+    .send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+    const request = await startup.methods.requests(0).call();
+    assert.equal('Buy shoes', request.description);
   });
 });
