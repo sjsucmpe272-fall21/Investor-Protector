343dc412cf77431ad5e62d4783c989f08044317a
jayanth <123jayanth123@gmail.com>
2021-12-01 11:13:58 -0800

    Landing Page Component

---
+++ pages/campaigns/landingPages.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Button, Card, Grid } from 'semantic-ui-react';
+import Campaign from '../../ethereum/campaign';
+import ContributeForm from '../../components/ContributeForm';
+import Layout from '../../components/Layout';
+import Link from 'next/link';
+import web3 from '../../ethereum/web3';
+
+class landingPages extends Component {
+    render() {
+        return (
+          <div>
+            <br />
+            <div className="justify-content-md-center">
+              
+            </div>
+            {/* </Container> */}
+          </div>
+        );
+    }    
+}
+
+export default landingPages;
