a5683ac20e7face740750e3206513c95d8b59d4b
jayanth <123jayanth123@gmail.com>
2021-12-01 11:14:39 -0800

    Landing Page Content

--- pages/campaigns/landingPages.js
+++ pages/campaigns/landingPages.js
@@ -12,7 +12,26 @@ class landingPages extends Component {
           <div>
             <br />
             <div className="justify-content-md-center">
-              
+              <span>
+                <img className="landing-page-image" src='../image' alt="Landing page image" fluid />
+              </span>
+              <span>
+                <br />
+                <br />
+                <center className="landing-page-button">
+                  <span className="heading-content">Welcome !</span>
+                  <br />
+                  <br />
+                  <div className="d-grid gap-4">
+                    <Button variant="primary" className="btn-primary" size="lg">
+                      <Link to="/login/customer">Start a new contribution..</Link>
+                    </Button>
+                    <Button variant="success" size="lg">
+                      <Link to="/login/restaurant">Startup Request..</Link>
+                    </Button>
+                  </div>
+                </center>
+              </span>
             </div>
             {/* </Container> */}
           </div>
